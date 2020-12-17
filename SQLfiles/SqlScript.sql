be0123b10ff2980748c2964db889b673c128aab5
Spencer Campbell <campbellspencer614@gmail.com>
2020-12-17 12:14:42 -0500

    added Sql file

---
+++ SQLfiles/SqlScript.sql
@@ -0,0 +1,23 @@
+create schema project2;
+
+create table person(
+	id serial primary key,
+	username varchar(64) not null,
+	pass varchar(64) not null,
+	skill_ranking integer not null,
+	profile_pic_path varchar(128)
+);
+
+create table game_state(
+	id serial primary key,
+	moveslist varchar(42) not null,
+	player_one_id integer references person
+	player_two_id integer references person
+);
+
+create table game_history(
+	id serial primary key,
+	date_played date not null,
+	winning_player_id integer references person
+	game_state_id integer references game_state
+);
