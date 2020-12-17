576a2d440557e150bf84d218acc3248a21d2a040
Spencer Campbell <campbellspencer614@gmail.com>
2020-12-17 12:33:36 -0500

    sqlfile changes

--- SQLfiles/SqlScript.sql
+++ SQLfiles/SqlScript.sql
@@ -1,6 +1,10 @@
 create schema project2;
 
-create table person(
+drop table project2.game_history;
+drop table project2.game_state;
+drop table project2.person;
+
+create table project2.person(
 	id serial primary key,
 	username varchar(64) not null,
 	pass varchar(64) not null,
@@ -8,16 +12,16 @@ create table person(
 	profile_pic_path varchar(128)
 );
 
-create table game_state(
+create table project2.game_state(
 	id serial primary key,
 	moveslist varchar(42) not null,
-	player_one_id integer references person
-	player_two_id integer references person
+	player_one_id integer references project2.person,
+	player_two_id integer references project2.person
 );
 
-create table game_history(
+create table project2.game_history(
 	id serial primary key,
 	date_played date not null,
-	winning_player_id integer references person
+	winning_player_id integer references person,
 	game_state_id integer references game_state
 );
