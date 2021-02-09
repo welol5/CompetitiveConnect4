a983de00c564593faade81e91e160595029f4e9e
welol5 <william.elliman@gmail.com>
2021-02-09 14:39:24 -0500

    Changed default server port

--- Frontend/competitive-connect-four/src/app/services/url.service.ts
+++ Frontend/competitive-connect-four/src/app/services/url.service.ts
@@ -7,10 +7,10 @@ export class UrlService {
 
   constructor() { }
   getUrl(): string {
-    return 'http://localhost:8080/CompetitiveConnect4/';
+    return 'http://localhost:6401/CompetitiveConnect4/';
   }
 
   getWSUrl(): string {
-    return 'ws://localhost:8080/CompetitiveConnect4/';
+    return 'ws://localhost:6401/CompetitiveConnect4/';
   }
 }
