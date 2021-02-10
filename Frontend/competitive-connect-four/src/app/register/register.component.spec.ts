17dc00d1cca1ede71ca0b0cd948e88ac8dcb0288
welol5 <william.elliman@gmail.com>
2021-02-10 10:40:01 -0500

    Added small changes to existing test

--- Frontend/competitive-connect-four/src/app/register/register.component.spec.ts
+++ Frontend/competitive-connect-four/src/app/register/register.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { by, element } from 'protractor';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let usernameField = element(by.id('user'));
+  let passwordField = element(by.id('pass'));
+  let registerButton = element(by.id('registerBtn'));
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,5 +25,8 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(usernameField).toBeTruthy();
+    expect(passwordField).toBeTruthy();
+    expect(registerButton).toBeTruthy();
   });
 });
