f3b83d07e8a6f6348ce151ac7cc2c0aaad60a698
welol5 <william.elliman@gmail.com>
2021-02-16 11:25:39 -0500

    started thinking about tests

--- Frontend/competitive-connect-four/src/app/login/login.component.spec.ts
+++ Frontend/competitive-connect-four/src/app/login/login.component.spec.ts
@@ -1,10 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { asyncData } from 'coverage/competitive-connect-four/src/testing/async-observable-helper';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { Person } from '../models/Person';
+import { PersonService } from '../services/person.service';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  let loginServiceSpy: jasmine.SpyObj<PersonService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,5 +28,19 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set return a Person who is logged in', () => {
+    loginServiceSpy = jasmine.createSpyObj('PersonService', ['loginPerson']);
+    let testPerson: Person = new Person();
+    testPerson.username = 'testUsername';
+    testPerson.password = 'testPassword';
+    loginServiceSpy.loginPerson.and.returnValue(asyncData(testPerson));
+
+    let component = new LoginComponent(loginServiceSpy as any);
+
+    component.user = 'testUsername';
+    component.pass = 'testPassword';
+    component.logIn();
   });
 });
